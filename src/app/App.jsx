8d99be4b4ea90342a1f940a9d5eb8e88532105f4
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-01-04 23:48:09 +0530

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Nic from '../pages/Nic/Nic'
+function App() {
+ 
+
+  return (
+    <div>
+      <Nic/>
+    </div>
+  
+  )
+}
+
+export default App
---
+++ src/pages/Nic/Nic.jsx
@@ -0,0 +1,55 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
+
+import lankaNIC from 'lanka-nic';
+
+
+export default function Nic(){
+  
+    const [nic,setnic]=useState("")
+    const [bday,setbday]=useState("")
+    const [gender,setgender]=useState("")
+
+    function search(){
+        console.log(nic);
+        let { dateOfBirth,gender}=lankaNIC.getInfoFromNIC(nic);
+        setbday(dateOfBirth.toDateString());
+        
+        setgender(gender);
+    }
+   function clear(){
+    setnic("")
+    setbday("")
+    setgender("")
+  
+   }
+  
+    return(
+   <div>
+     <Card  className='m-auto  mt-5' style={{ width: '30rem' }}>
+    
+      <Card.Body>
+        <Card.Title>NIC Application</Card.Title>
+        <Card.Text>
+        <Form.Control value={nic} className="mt-4" type="text"  placeholder="Input Your NIC No" onChange={(val)=>setnic(val.target.value)}/>
+        
+        </Card.Text>
+        <div>
+        <Button className='mt-3'   variant="danger" onClick={search}>Submit</Button>
+        </div>
+      
+        <h5 className='mt-3'>Birth day :{bday}</h5>
+        <h5 className='mt-2'>Gender :{gender}</h5>
+        
+        <div>
+       
+        <Button  className='ms-5'   variant="primary" onClick={clear}>Clear</Button>
+        </div> 
+        
+      </Card.Body>
+    </Card>
+</div>
+    )
+} 
